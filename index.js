d2178e55b981490f5555acdd8e7b6999e1d09710
Bethany Oestrich <bethany@oestrich.org>
2018-04-07 15:29:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(array,name){
-   array.push(name)
-   return array}
+function destructivelyAppendKitten(kittens,name){
+   kittens.push(name)
+   return kittens}
